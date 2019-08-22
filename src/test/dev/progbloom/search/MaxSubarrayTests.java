abb1798002545bf8b3b41d14e8dd9ffc7934fe8a
blinky <dm.blink19@gmail.com>
2019-08-22 13:23:36 +0300

    Add new max subarray test

--- src/test/dev/progbloom/search/MaxSubarrayTests.java
+++ src/test/dev/progbloom/search/MaxSubarrayTests.java
@@ -34,6 +34,14 @@ class MaxSubarrayTests {
 
     @ParameterizedTest
     @MethodSource("provideMaxSubarrayAlgorithms")
+    void givenLongArrayAndOddLength_findMaxSubarray_shouldReturnProperSubarray(AlgorithmMaxSubarray alg) {
+        int[] arr = new int[]{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7, 2};
+        MaxSubarrayResult properResult = new MaxSubarrayResult(7, 10, 43);
+        assertThat(alg.findMaxSubarray(arr, 0, arr.length - 1)).isEqualToComparingFieldByField(properResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideMaxSubarrayAlgorithms")
     void givenTwoMaxEqualAndPreviousByValueNumber_findMaxSubarray_shouldReturnMaxValue(AlgorithmMaxSubarray alg) {
         // this test checks comparing equal values. Two equal numbers here are: -3, -3, and prev by value: -4
         int[] arr = new int[]{-13, -3, -25, -20, -3, -16, -23, -18, -20, -7, -12, -5, -22, -15, -4, -7};
