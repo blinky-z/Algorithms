6ee3a2a84148ec7060d2b8259eb85a4f157c18e1
Blinky <dm.blink19@gmail.com>
2019-09-09 23:30:13 +0300

    Add binary search tree

---
+++ src/main/dev/progbloom/datastruct/TreeNode.java
@@ -0,0 +1,25 @@
+package dev.progbloom.datastruct;
+
+/**
+ * Tree node representation.
+ */
+public class TreeNode {
+    int key;
+
+    TreeNode left;
+
+    TreeNode right;
+
+    TreeNode p;
+
+    public TreeNode(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "key=" + key +
+                '}';
+    }
+}
